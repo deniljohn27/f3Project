da79fcc2dd7f313405f2a365222026ef4e5eb1c4
denil <deniljohnms27@gmail.com>
2023-07-02 00:41:57 +0530

    v4

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let login=document.getElementById("login");
 let sign=document.getElementById("signup");
 
 login.addEventListener("click",()=>{
-    window.location.href="./login/index.html"
+    if(localStorage.getItem("users")){
+        window.location.href="./login/index.html"
+    }else{
+        alert("No Users Present SignUp to continue");
+        window.location.href="./signUp/index.html"
+    }
+    
 });
 
 sign.addEventListener("click",()=>{
